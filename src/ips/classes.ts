94e261eda23a138165c6a8c9fd3ab74d1b9ec00b
Mike <michael.johanson@intel.com>
2021-12-23 09:16:42 -0700

    refactor(wsman): initial migration
    addresses AB#4559

---
+++ src/ips/classes.ts
@@ -0,0 +1,9 @@
+/*********************************************************************
+* Copyright (c) Intel Corporation 2021
+* SPDX-License-Identifier: Apache-2.0
+**********************************************************************/
+
+export enum Classes {
+  IPS_OPT_IN_SERVICE = 'IPS_OptInService',
+  IPS_HOST_BASED_SETUP_SERVICE = 'IPS_HostBasedSetupService'
+}
