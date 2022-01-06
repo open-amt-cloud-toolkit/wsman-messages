7bb6840f14eaf7e5adc99a2869f3a8b2408936b6
Mike <michael.johanson@intel.com>
2022-01-06 16:13:49 -0700

    ci(build): remove dist folder

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import * as AMT from './amt';
-import * as IPS from './ips';
-import * as CIM from './cim';
-import * as Common from './models';
-export { AMT, IPS, CIM, Common };
--- dist/models/amt_models.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/*********************************************************************
-* Copyright (c) Intel Corporation 2021
-* SPDX-License-Identifier: Apache-2.0
-**********************************************************************/
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=amt_models.js.map
