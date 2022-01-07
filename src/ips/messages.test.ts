80e0e6fe6cca5fb08f3f39295bf9155de86b0e06
matt-primrose <matt.c.primrose@intel.com>
2022-01-07 08:09:37 -0700

    test(ips_messages): adds test coverage

--- src/ips/messages.test.ts
+++ src/ips/messages.test.ts
@@ -6,6 +6,7 @@
 import { Messages } from './messages'
 import { Methods } from './methods'
 import { WSManErrors } from '../WSMan'
+import { IPS_OptInService, IPS_OptInServiceResponse } from '../models/ips_models'
 
 const ipsClass = new Messages()
 const castedIPSClass = new Messages() as any
@@ -25,6 +26,32 @@ describe('IPS Tests', () => {
     })
     it('should throw error if an unsupported method is called', () => {
       expect(() => { castedIPSClass.OptInService(Methods.REQUEST_POWER_STATE_CHANGE, messageId) }).toThrow(WSManErrors.UNSUPPORTED_METHOD)
+    })
+    it('should create a valid ips_OptInService Put wsman message', () => {
+      const data: IPS_OptInServiceResponse = {
+        IPS_OptInService: {
+          OptInCodeTimeout: 300
+        }
+      }
+      const response = ipsClass.OptInService(Methods.PUT, messageId, null, data)
+      const correctResponse = '<?xml version="1.0" encoding="utf-8"?><Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:w="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><a:Action>http://schemas.xmlsoap.org/ws/2004/09/transfer/Put</a:Action><a:To>/wsman</a:To><w:ResourceURI>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService</w:ResourceURI><a:MessageID>1</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><w:OperationTimeout>PT60S</w:OperationTimeout></Header><Body><r:IPS_OptInService xmlns:r="http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService"><r:OptInCodeTimeout>300</r:OptInCodeTimeout></r:IPS_OptInService></Body></Envelope>'
+      expect(response).toEqual(correctResponse)
+    })
+    it('should create a valid ips_StartOptIn wsman message', () => {
+      const response = ipsClass.OptInService(Methods.START_OPT_IN, messageId)
+      const correctResponse = '<?xml version="1.0" encoding="utf-8"?><Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:w="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><a:Action>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/StartOptIn</a:Action><a:To>/wsman</a:To><w:ResourceURI>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService</w:ResourceURI><a:MessageID>1</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><w:OperationTimeout>PT60S</w:OperationTimeout></Header><Body><r:StartOptIn_INPUT xmlns:r="http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService" /></Body></Envelope>'
+      expect(response).toEqual(correctResponse)
+    })
+    it('should create a valid ips_SendOptInCode wsman message', () => {
+      const code = 1
+      const response = ipsClass.OptInService(Methods.SEND_OPT_IN_CODE, messageId, code)
+      const correctResponse = '<?xml version="1.0" encoding="utf-8"?><Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:w="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><a:Action>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/SendOptInCode</a:Action><a:To>/wsman</a:To><w:ResourceURI>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService</w:ResourceURI><a:MessageID>1</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><w:OperationTimeout>PT60S</w:OperationTimeout></Header><Body><r:SendOptInCode_INPUT xmlns:r="http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService"><r:OptInCode>1</r:OptInCode></r:SendOptInCode_INPUT></Body></Envelope>'
+      expect(response).toEqual(correctResponse)
+    })
+    it('should create a valid ips_CancelOptIn wsman message', () => {
+      const response = ipsClass.OptInService(Methods.CANCEL_OPT_IN, messageId)
+      const correctResponse = '<?xml version="1.0" encoding="utf-8"?><Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing" xmlns:w="http://schemas.dmtf.org/wbem/wsman/1/wsman.xsd" xmlns="http://www.w3.org/2003/05/soap-envelope"><Header><a:Action>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/CancelOptIn</a:Action><a:To>/wsman</a:To><w:ResourceURI>http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService</w:ResourceURI><a:MessageID>1</a:MessageID><a:ReplyTo><a:Address>http://schemas.xmlsoap.org/ws/2004/08/addressing/role/anonymous</a:Address></a:ReplyTo><w:OperationTimeout>PT60S</w:OperationTimeout></Header><Body><r:CancelOptIn_INPUT xmlns:r="http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService" /></Body></Envelope>'
+      expect(response).toEqual(correctResponse)
     })
   })
   describe('ips_HostBasedSetupService Tests', () => {
