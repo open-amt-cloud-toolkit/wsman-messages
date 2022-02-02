d7a6502f7230ed29284ca38f9e3f89bade8e17c6
madhavilosetty-intel <madhavi.losetty@intel.com>
2022-02-02 10:41:18 -0800

    fix(ips): updates methods and actions for HostBasedService

--- src/ips/actions.ts
+++ src/ips/actions.ts
@@ -10,8 +10,8 @@ export enum Actions {
   PUT = 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Put',
   DELETE = 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Delete',
   SETUP = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/Setup',
-  ADMIN_SETUP = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AdminSetupResponse',
-  ADD_NEXT_CERT_IN_CHAIN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AddNextCertInChainResponse',
+  ADMIN_SETUP = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AdminSetup',
+  ADD_NEXT_CERT_IN_CHAIN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AddNextCertInChain',
   START_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/StartOptIn',
   CANCEL_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/CancelOptIn',
   SEND_OPT_IN_CODE = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/SendOptInCode'
