53ba5a1c2f45bdeabd35c1aed9611eaa7f9b4167
matt-primrose <matt.c.primrose@intel.com>
2022-12-15 11:25:52 -0700

    docs(ips): add value map descriptions to types

--- src/ips/types.ts
+++ src/ips/types.ts
@@ -5,18 +5,42 @@
 
 export namespace Types {
   export namespace HostBasedSetupService {
+    /**
+     * Not provisioned:0 | Client:1 | Admin:2
+     */
     export type CurrentControlMode = 0 | 1 | 2
+    /**
+     * Not Started:0 | Chain In-Progress:1 | Chain Complete:2
+     */
     export type CertChainStatus = 0 | 1 | 2
+    /**
+     * None:0 | Other:1 | HTTP Digest MD5(A1):2
+     */
     export type AdminPassEncryptionType = 0 | 1 | 2
+    /**
+     * None:0 | Other:1 | RSA_SHA-2_256:2
+     */
     export type SigningAlgorithm = 0 | 1 | 2
   }
   export namespace OptInService {
-    export type OptInRequired = 0 | 1
+    /**
+     * None:0 | KVM:1 | All:4294967295
+     */
+    export type OptInRequired = 0 | 1 | 4294967295
+    /**
+     * Not started:0 | Requested:1 | Displayed:2 | Received:3 | In Session:4
+     */
     export type OptInState = 0 | 1 | 2 | 3 | 4
+    /**
+     * FALSE:0 | TRUE:1
+     */
     export type CanModifyOptInPolicy = 0 | 1
   }
 
   export namespace IEEE8021xSettings {
+    /**
+     * Enabled:2 | Disabled:3 | Enabled Without Certificates:6
+     */
     export type Enabled = 2 | 3 | 6
   }
 }
