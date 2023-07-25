35715fa14fce3eff3436492a5cf162fe654a7d3f
madhavilosetty-intel <madhavi.losetty@intel.com>
2023-07-25 10:12:06 -0700

    feat: adds call for UpgradeClientToAdmin

--- src/ips/actions.ts
+++ src/ips/actions.ts
@@ -6,6 +6,7 @@
 export enum Actions {
   SETUP = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/Setup',
   ADMIN_SETUP = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AdminSetup',
+  UPGRADE_CLIENT_TO_ADMIN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/UpgradeClientToAdmin',
   ADD_NEXT_CERT_IN_CHAIN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_HostBasedSetupService/AddNextCertInChain',
   START_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/StartOptIn',
   CANCEL_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/CancelOptIn',
